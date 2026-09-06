c5b43c556b1d0235096c645d65138957b212d9dc
pabellis <mail@bellissantpablo.fr>
2026-09-06 18:17:36 +0200

    fix: Change __FUNCTION__ to __func__

--- include/simply_error.h
+++ include/simply_error.h
@@ -6,10 +6,10 @@ void	register_error(const char *file_name, int line, const char *function, const
 #ifdef NDEBUG
 	#define REGISTER_ERROR(...) ((void *)0)
 #else
-	#define REGISTER_ERROR(...) register_error(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+	#define REGISTER_ERROR(...) register_error(__FILE__, __LINE__, __func__, __VA_ARGS__)
 #endif
 
-#define REGISTER_USER_ERROR(...) register_error(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define REGISTER_USER_ERROR(...) register_error(__FILE__, __LINE__, __func__, __VA_ARGS__)
 
 #define CHECK_ERROR(expr, ...) if (expr) {REGISTER_ERROR(__VA_ARGS__); return (-1);}
 #define CHECK_USER_ERROR(expr, ...) if (expr) {REGISTER_USER_ERROR(__VA_ARGS__); return (-1);}
