5fb7245696e9ecaf908dc8dcf39d7eb396e8380a
pabellis <mail@bellissantpablo.fr>
2026-09-06 15:41:31 +0200

    feat: Add user error, and set toggeleable classic error with ndebug

--- include/simply_error.h
+++ include/simply_error.h
@@ -3,4 +3,10 @@
 __attribute__((format(printf, 4, 5)))
 void	register_error(const char *file_name, int line, const char *function, const char *format, ...);
 
-#define REGISTER_ERROR(...) register_error(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#ifdef NDEBUG
+	#define REGISTER_ERROR(...) ((void *)0)
+#else
+	#define REGISTER_ERROR(...) register_error(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#endif
+
+#define REGISTER_USER_ERROR(...) register_error(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
